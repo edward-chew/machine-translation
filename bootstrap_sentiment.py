e03dd3a1f8ea4f7a8f42c214b8fc372b8b94ed70
Edward Chew <ewchew@ucdavis.edu>
2022-11-12 15:52:51 -0800

    Add word embeddings

--- bootstrap_sentiment.py
+++ bootstrap_sentiment.py
@@ -8,47 +8,57 @@ import argparse
 import csv
 
 # The main routine
-def main(dir_name: str, poly_label_col: str, true_label_col: str) -> None:
+def main(dir_name: str, poly_label_col: str, true_label_col: str, skip_english: bool = False) -> None:
   accuracies = []
   noneu_accuracies = []
 
   # Bootstrap each language
   for rf in sorted(os.listdir(dir_name)):
+    print(rf)
+
+    if skip_english and rf == "English.csv":
+      print("    Skipping English...")
+      continue
+
     df = pd.read_csv(dir_name + "/" + rf)
 
-    accuracies, noneu_accuracies = bootstrap(df, poly_label_col, true_label_col, sample_size=10000)
+    accuracies, noneu_accuracies = bootstrap(df, poly_label_col, true_label_col, sample_size=0)
 
     # Save the accuracies to csv
-    path = f"Bootstrapped/{dir_name.split('_')[0]}/AllTweets/"
+    path = f"{dir_name}_BootstrappedResults/{poly_label_col}/AllTweets/"
     os.makedirs(path, exist_ok=True)  # Create the directory if it doesn't exist
-    with open(path + rf.split('_')[0] + ".csv", "w") as wf:
+    with open(path + rf, "w") as wf:
       writer = csv.writer(wf)
       writer.writerow(accuracies)
 
-    path = f"Bootstrapped/{dir_name.split('_')[0]}/NoNeuTweets/"
+    path = f"{dir_name}_BootstrappedResults/{poly_label_col}/NoNeuTweets/"
     os.makedirs(path, exist_ok=True)
-    with open(path + rf.split('_')[0] + ".csv", "w") as wf:
+    with open(path + rf, "w") as wf:
       writer = csv.writer(wf)
       writer.writerow(noneu_accuracies)
 
   # Bootstrap all languages combined
   # Go through all the files, put all the files in a single dataframe
+  print("Combined")
   df_list = []
   for rf in sorted(os.listdir(dir_name)):
+    if skip_english and rf == "English.csv":
+      print("    Skipping English...")
+      continue
     temp_df = pd.read_csv(dir_name + "/" + rf)
     df_list.append(temp_df)
-    combined_df = pd.concat(df_list, axis=0, ignore_index=True)
+  combined_df = pd.concat(df_list, axis=0, ignore_index=True)
 
   accuracies, noneu_accuracies = bootstrap(combined_df, poly_label_col, true_label_col)
 
   # Save the accuracies to csv
-  path = f"Bootstrapped/{dir_name.split('_')[0]}/AllTweets/"
+  path = f"{dir_name}_BootstrappedResults/{poly_label_col}/AllTweets/"
   os.makedirs(path, exist_ok=True)
   with open(path + "Combined.csv", "w") as wf:
     writer = csv.writer(wf)
     writer.writerow(accuracies)
 
-  path = f"Bootstrapped/{dir_name.split('_')[0]}/NoNeuTweets/"
+  path = f"{dir_name}_BootstrappedResults/{poly_label_col}/NoNeuTweets/"
   os.makedirs(path, exist_ok=True)
   with open(path + "Combined.csv", "w") as wf:
     writer = csv.writer(wf)
@@ -83,13 +93,16 @@ if __name__ == "__main__":
   # parser.add_argument("file_directory", default="Twitter Dataset_CleanOutput", help="Name of the directory the tweet files are in")
   # parser.add_argument("poly_label_column_name", default="Tweet text_Clean_Label", help="Name of the column the Polyglot label is in")
   # parser.add_argument("true_label_column_name", default="SentLabel", help="Name of column the correct label is in")
+  # parser.add_argument("skip_english", default=False, help="Whether to skip the English.csv file")
   # args = parser.parse_args()
 
   # dir_name = args.file_directory
   # poly_label_col = args.poly_label_column_name
   # true_label_col = args.true_label_column_name
+  # skip_english = args.skip_english
 
-  dir_name = "Twitter Dataset New Languages_CleanOutput_10000Sample_EnglishToOriginal_PolyglotSentimentOutput"  # Name of the directory the tweet files are in
+  dir_name = "0_AllTranslationsCombined_Sampled_PolyglotSentimentOutput"  # Name of the directory the tweet files are in
   poly_label_col = "ReverseTrans_Label"  # Name of the column the polyglot label is in
   true_label_col = "SentLabel"  # Name of column the correct label is in
-  main(dir_name, poly_label_col, true_label_col)
+  skip_english = True # If English should be skipped
+  main(dir_name, poly_label_col, true_label_col, skip_english)
